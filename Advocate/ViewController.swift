d0b012b015512702e3d514a76f6a753d9539e1a2
DavidOliverDoswell <davidoliverdoswell@icloud.com>
2018-03-16 18:02:50 -0400

    Initial Commit

---
+++ Advocate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Advocate
+//
+//  Created by David Oliver Doswell on 3/16/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
